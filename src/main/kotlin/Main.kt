9161f2260aa533bf9c48ec4055b35f7e865f7f22
DESKTOP-8NG6QPL\gomez <gomezrondon@gmail.com>
2018-06-02 08:09:28 -0600

    Removing unnecessary java dependency on gradle, adding test file and source script on resources folder.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 import java.io.File
 
 fun main(args : Array<String>) {
+
+    if (args.size <= 0 ) return System.exit(1)
+
     var stashName = args[0].toString().toLowerCase()
 
     val lines = File(stashName).readLines()
@@ -11,3 +14,7 @@ fun main(args : Array<String>) {
             .map{it.replace("* ","")}
             .forEach { println(it) }
 }
+
+
+//how to execute
+// java -jar current_branch.jar list-branchs.txt
