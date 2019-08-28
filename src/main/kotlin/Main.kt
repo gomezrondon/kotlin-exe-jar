dc4b18e8c9ecb1862633be399b7ca6a81b779d8c
cajgomez <Jgomez@cambridgeassociates.com>
2019-08-28 10:36:25 -0600

    kotlin 3 for loop ways

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 import java.io.File
 
 fun main() {
-    println("Hola Mundo!")
+
+    val primes = listOf<Int>(1, 3 , 5,7,9,13)
+
+    primes.forEachIndexed { i, prime -> println("Prime #${i+1} is: $prime")}
+    primes.stream().map { it }
 }
 
 
