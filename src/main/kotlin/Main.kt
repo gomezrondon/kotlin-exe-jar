8570360264b46a8c8ac03f0aa4d6aaa642d9b26a
cajgomez <Jgomez@cambridgeassociates.com>
2019-09-04 16:18:26 -0600

    A script to open/close a program if its process is running.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,41 @@
 import java.io.File
+import java.lang.ProcessBuilder.Redirect
+import java.util.concurrent.TimeUnit
+
+//"cmd.exe /c gradle clean build".runCommand(timeout = 60)
 
 fun main() {
+    File("salida.txt").writeText("empty")
 
-    val primes = listOf<Int>(1, 3 , 5,7,9,13)
+    "cmd.exe /c tasklist".runCommand(timeout = 60)
+    val filter = File("salida.txt").readLines().filter { it.startsWith("SnippingTool.exe") }
 
-    primes.forEachIndexed { i, prime -> println("Prime #${i+1} is: $prime")}
-    primes.stream().map { it }
+    if (filter.size > 0) {
+        println("la aplicacion existe!, I will kill it")
+        Runtime.getRuntime().exec("cmd.exe /c taskkill /F /IM SnippingTool.exe");
+    }else{
+        println("Ejecutando aplicacion")
+        Runtime.getRuntime().exec("cmd.exe /c start %windir%\\system32\\SnippingTool.exe");// execute and continue in another thread
+    }
+
 }
 
+fun String.runCommand(workingDir: File? = null, timeout:Long) {
+    val process = ProcessBuilder(*split(" ").toTypedArray())
+            .directory(workingDir)
+            .redirectOutput(Redirect.appendTo(File("salida.txt")))
+            .redirectError(Redirect.INHERIT)
+            .start()
 
-//how to execute
-// java -jar current_branch.jar list-branchs.txt
+    // File("salida.txt").readLines().forEach { println(it) }
+
+    if (!process.waitFor(timeout, TimeUnit.SECONDS)) {
+        process.destroy()
+        throw RuntimeException("execution timed out: $this")
+    }
+    if (process.exitValue() != 0) {
+        throw RuntimeException("execution failed with code ${process.exitValue()}: $this")
+    }
+
+
+}
