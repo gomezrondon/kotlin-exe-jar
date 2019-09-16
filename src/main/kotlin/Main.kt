6d8ea46ed55c3bb2f060963a8bee5be6485a59e4
cajgomez <Jgomez@cambridgeassociates.com>
2019-09-16 16:38:55 -0600

    Search text 1rst commit.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,41 +1,50 @@
 import java.io.File
-import java.lang.ProcessBuilder.Redirect
-import java.util.concurrent.TimeUnit
 
-//"cmd.exe /c gradle clean build".runCommand(timeout = 60)
+//C:\temp
 
 fun main() {
-    File("salida.txt").writeText("empty")
 
-    "cmd.exe /c tasklist".runCommand(timeout = 60)
-    val filter = File("salida.txt").readLines().filter { it.startsWith("SnippingTool.exe") }
+    ///------------ parte 1 indexa
+    val folder = """C:\Users\jrgm\Documents"""
+    //val folder = "C:\\temp"
+    "C:\\CA"
+    //C:\Users\jrgm\Documents
+    val indexFile = File("index.txt")
+    if (indexFile.exists()){
+        indexFile.delete()
+    }
 
-    if (filter.size > 0) {
-        println("la aplicacion existe!, I will kill it")
-        Runtime.getRuntime().exec("cmd.exe /c taskkill /F /IM SnippingTool.exe");
-    }else{
-        println("Ejecutando aplicacion")
-        Runtime.getRuntime().exec("cmd.exe /c start %windir%\\system32\\SnippingTool.exe");// execute and continue in another thread
+    File("index.txt").bufferedWriter().use { out ->
+        File(folder).walkTopDown().filter { it.isFile }.forEach { out.write(it.name +","+ it.absolutePath + "\n") }
+    }
+  ///------------ parte 2
+    val regexExec ="""^.*angu.*""".toRegex(RegexOption.MULTILINE)
+
+    val lines = File("index.txt").readLines()
+
+    val search_result = lines.flatMap { regexExec.findAll(it).map { it.value }.toList() }
+            .map { it.replace("* ", "") }
+
+    val size = search_result.size
+
+//    var max = search_result.map { it.length }.max() ?: 0
+    var max = 30
+
+    if (File("search_result.txt").exists()){
+        File("search_result.txt").delete()
+    }
+
+    File("search_result.txt").bufferedWriter().use { out ->
+
+        search_result.forEach {
+            val arre = it.split(",")
+            val repeat= (max + 15) - arre.get(0).length
+            val line = arre.get(0) + " ".repeat(repeat) + arre.get(1) + "\n"
+            out.write(line)
+
+        }
+        out.write("Num. Results: $size")
     }
 
 }
 
-fun String.runCommand(workingDir: File? = null, timeout:Long) {
-    val process = ProcessBuilder(*split(" ").toTypedArray())
-            .directory(workingDir)
-            .redirectOutput(Redirect.appendTo(File("salida.txt")))
-            .redirectError(Redirect.INHERIT)
-            .start()
-
-    // File("salida.txt").readLines().forEach { println(it) }
-
-    if (!process.waitFor(timeout, TimeUnit.SECONDS)) {
-        process.destroy()
-        throw RuntimeException("execution timed out: $this")
-    }
-    if (process.exitValue() != 0) {
-        throw RuntimeException("execution failed with code ${process.exitValue()}: $this")
-    }
-
-
-}
