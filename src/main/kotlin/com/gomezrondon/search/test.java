4b1820dab73e271a3f20a96d0952dcbc0454a3d4
cajgomez <Jgomez@cambridgeassociates.com>
2019-09-23 17:05:31 -0600

    Almost Finished.

--- src/main/kotlin/com/gomezrondon/search/test.java
+++ src/main/kotlin/com/gomezrondon/search/test.java
@@ -2,24 +2,24 @@ package com.gomezrondon.search;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.stream.BaseStream;
-
 import static com.gomezrondon.search.IndexerKt.loadFolders;
 
 public class test {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)  {
 
         var folders = loadFolders();
 
-        String fileName = "repository/output_temp.txt";
+        String fileName = "repository/output_internet explorer.txt";
 
         Flux<String> stringFlux = null;
         try {
@@ -41,8 +41,29 @@ public class test {
 
 
         for (List<String> line : block) {
-            System.out.println(line);
-            System.out.println("-----------------------------");
+            String regex = "Directory of ";
+            String[] split = line.stream().findFirst().get().split(regex);
+            String directory = split[1];
+
+            String level = SplitWorkLoadKt.getPathByLevel(6, directory);
+//            Files.write(Paths.get("index_ie.txt"), "".getBytes());
+
+            if (level.equals("null")) { // WTF returns a string call null
+                line.stream().skip(1)
+                        .map(x -> x.replaceAll("(\\s){2,20}", " "))
+                        .map(x -> x.split(" "))
+                        .map(arre -> arre[4] + "," + directory + File.separator + arre[4] + "," + arre[0] + " " + arre[1] + " " + arre[2]+"\n")
+                        //.filter(it ->)
+                        .forEach(linea -> {
+                            try {
+                                Files.write(Paths.get("index_ie.txt"), linea.getBytes(), StandardOpenOption.APPEND);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        });
+            }
+
+
         }
         System.out.printf(">>>>>>>>");
     }
