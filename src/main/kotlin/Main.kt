ffa591eec6c2be573cfe87d7098e281e1ac76562
cajgomez <Jgomez@cambridgeassociates.com>
2019-09-24 14:56:50 -0600

    Adding reset option and timing execution.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,19 @@ import com.gomezrondon.search.combineAllResults
 import com.gomezrondon.search.indexFiles
 import com.gomezrondon.search.loadFolders
 import com.gomezrondon.search.parallaleSearch
+import java.io.File
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+import kotlin.system.measureTimeMillis
 
 fun main(array: Array<String>) {
     val folders = loadFolders()
 
     when(array[0].toInt()) {
+        0 -> { // index files in folders
+            resetRepository()
+        }
+
         1 -> { // index files in folders
             indexar(folders)
         }
@@ -23,11 +29,27 @@ fun main(array: Array<String>) {
     }
 }
 
+fun resetRepository() {
+    File("repository").walkTopDown()
+            .filter { it.isFile }
+            .filter { it.name.startsWith("md5_") }
+            .forEach {
+                println("DELETING $it !!!")
+                    it.delete()
+            }
+    println("Finish Reseting...")
+}
+
 private fun indexar(folders: List<String>) {
     val runnable = {
-        println("Start Indexing!")
-        indexFiles(folders)
-        println("Done Indexing!")
+
+        val time = measureTimeMillis {
+            println("Start Indexing!")
+            indexFiles(folders)
+            println("Done Indexing!")
+        }
+        println("Exe. Time: $time")
+
     }
     Executors.newScheduledThreadPool(1).scheduleAtFixedRate(runnable, 1, 60, TimeUnit.SECONDS)
 }
