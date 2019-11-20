e4ccc84e3f7c132b6bab227805c0aa4cf2c1184f
cajgomez <Jgomez@cambridgeassociates.com>
2019-11-20 16:47:19 -0600

    1rst Commit.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,45 @@
 import java.io.File
 
-fun main() {
 
-    val primes = listOf<Int>(1, 3 , 5,7,9,13)
+fun main(arg: Array<String>) {
+    val textFileList = listOf("pas")
+    val listPasFiles = loadFolders(arg.get(0))
+            .filter { textFileList.contains(it.extension.toLowerCase()) }
 
-    primes.forEachIndexed { i, prime -> println("Prime #${i+1} is: $prime")}
-    primes.stream().map { it }
+    val listOfWords = arg
+
+    val resultSet: List<String?> = listPasFiles.map { Paquete(it, it.readLines()) }
+            .map {
+                val absolutePath = it.file.absolutePath
+                val toList = it.lines.mapIndexed { index, line ->
+                    val first = listOfWords.filter { word -> line.contains(word) }
+                            .map { "$index: $line | " + absolutePath }
+                            .firstOrNull()
+
+                    first
+
+                }.filter { !it.isNullOrEmpty() }
+                toList.toTypedArray()
+            }.flatMap { it.asSequence() }.toList()
+
+    val max = resultSet.map { it!!.split("|") }
+            .map { it.get(0) }
+            .map { it.length }
+            .max()?.or(0)
+
+    resultSet.forEach {
+        val split = it!!.split("|")
+        val parte1 = split.get(0)
+        val parte2 = split.get(1)
+
+        println("$parte1 ${max?.let { it1 -> " ".repeat(it1.minus(parte1.length)) }} $parte2")
+
+    }
+
+
+
 }
 
 
-//how to execute
-// java -jar current_branch.jar list-branchs.txt
+data class Paquete(val file:File, var lines:List<String> )
+fun loadFolders(path:String) = File(path).walkTopDown().filter { it.isFile }
